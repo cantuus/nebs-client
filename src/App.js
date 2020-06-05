d64e44fd7bf9c2ee8bcd18ea203894329c494e67
Sophia Koeut <koeutsophia@gmail.com>
2020-06-04 20:35:58 -0700

    added addfolder and addbookmark component separately

--- src/App.js
+++ src/App.js
@@ -1,193 +1,153 @@
-import React from 'react';
-import { Route, Link, Switch } from 'react-router-dom'
-import config from './config'
-import Context from './Context'
-import BookmarkList from './components/MainScreen/Bookmarklist'
-import WholeBookmark from './components/Bookmark/WholeBookmark'
-import FolderList from './components/NavBar/Folderlist'
-import BookmarkNav from './components/NavBar/BookmarkNav'
-import AddFolder from './components/Folder/AddFolder'
-import AddBookmark from './components/Bookmark/AddBookmark'
-import ErrorPage from './components/MainScreen/Errorpage'
-import LoadingScreen from './components/MainScreen/Loadingscreen'
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import NoteListNav from '../NoteListNav/NoteListNav'
+import NotePageNav from '../NotePageNav/NotePageNav'
+import NoteListMain from '../NoteListMain/NoteListMain'
+import NotePageMain from '../NotePageMain/NotePageMain'
+import AddFolder from '../AddFolder/AddFolder'
+import AddNote from '../AddNote/AddNote'
+import ApiContext from '../ApiContext'
+import config from '../config'
+import './App.css'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      folders: [],
-      bookmarks: [],
-      loading: true,
-      error: null
-    }
-  }
+class App extends Component {
+  state = {
+    notes: [],
+    folders: [],
+  };
 
   componentDidMount() {
-    const options = {
-      method: 'GET',
-      headers: {
-        'content-type': 'application/json',
-        'Authorization': `Bearer ${config.API_KEY}`
-      }
-    }
     Promise.all([
-      fetch(`${config.API_ENDPOINT}folders`, options),
-      fetch(`${config.API_ENDPOINT}bookmarks`, options)
+      fetch(`${config.API_ENDPOINT}/notes`),
+      fetch(`${config.API_ENDPOINT}/folders`)
     ])
-      .then(([folders, bookmarks]) => {
-        if (!folders.ok) {
-          return folders.json().then(event => Promise.reject(event));
-        }
-        if (!bookmarks.ok) {
-          return bookmarks.json().then(event => Promise.reject(event));
-        }
+      .then(([notesRes, foldersRes]) => {
+        if (!notesRes.ok)
+          return notesRes.json().then(e => Promise.reject(e))
+        if (!foldersRes.ok)
+          return foldersRes.json().then(e => Promise.reject(e))
+
         return Promise.all([
-          folders.json(),
-          bookmarks.json()
+          notesRes.json(),
+          foldersRes.json(),
         ])
       })
-      .then(([foldersJson, bookmarksJson]) => {
-        this.setState({
-          folders: foldersJson,
-          bookmarks: bookmarksJson,
-          loading: false
-        })
+      .then(([notes, folders]) => {
+        this.setState({ notes, folders })
       })
       .catch(error => {
-        this.setState({ error: error.message });
-      });
-  }
-
-  renderMainComponent = () => {
-    return (
-      <>
-        {this.state.error ? <ErrorPage error={this.state.error} /> : !this.state.loading ? (this.renderMainRoutes()) : <LoadingScreen />}
-      </>
-    );
-  }
-
-
-  renderMainRoutes = () => {
-    return (<Switch>
-      <Route
-        exact path="/"
-        component={BookmarkList}
-      />
-      <Route
-        path="/folder/:folderId"
-        component={BookmarkList}
-      />
-      <Route
-        path="/bookmark/:bookmarkId"
-        component={WholeBookmark}
-      />
-      <Route
-        path="/AddBookmark"
-        component={AddBookmark}
-      />
-      <Route
-        path="/AddFolder"
-        component={AddFolder}
-      />
-      <Route render={() => <div>Something went wrong!</div>} />
-    </Switch>);
-  }
-
-
-
-
-  renderNavigationComponent = () => {
-    return (
-      <>
-        {!this.state.loading && <Switch>
-          <Route
-            path="/bookmark/:bookmarkId"
-            component={BookmarkNav}
-          />
-          <Route
-            path="/folder/:folderId"
-            component={FolderList}
-          />
-          <Route
-            exact path="/"
-            component={FolderList}
-          />
-          <Route
-            path="/AddFolder"
-            component={BookmarkNav}
-          />
-          <Route
-            path="/AddBookmark"
-            component={BookmarkNav}
-          />
-          <Route component={BookmarkNav} />
-        </Switch>}
-      </>
-    );
-  }
-
-  handleDeleteBookmark = (bookmarkId) => {
-    this.setState({
-      bookmarks: this.state.bookmarks.filter(bookmark => bookmark.id !== bookmarkId)
-    });
-  }
-
-  handleDeleteFolder = (folderId) => {
-    this.setState({
-      folders: this.state.folders.filter(folder => folder.id !== folderId),
-      bookmarks: this.state.bookmarks.filter(bookmark => bookmark.folder_id !== folderId)
-    });
-  }
-
-  handleUpdateFolder = (updatedFolder) => {
-    console.log(updatedFolder, this.state.folders);
-    this.setState({
-      folders: this.state.folders.map(folder => {
-        return (folder.id !== updatedFolder.id) ? folder : updatedFolder;
+        console.error({ error })
       })
-    });
   }
 
-  addFolder = (folder) => {
-    const folders = [...this.state.folders, folder]
+  handleAddFolder = folder => {
     this.setState({
-      folders
+      folders: [
+        ...this.state.folders,
+        folder
+      ]
     })
   }
 
-  addBookmark = (bookmark) => {
-    const bookmarks = [...this.state.bookmarks, bookmark]
+  handleAddNote = note => {
     this.setState({
-      bookmarks
+      notes: [
+        ...this.state.notes,
+        note
+      ]
     })
   }
 
+  handleDeleteNote = noteId => {
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== noteId)
+    })
+  }
+
+  renderNavRoutes() {
+    return (
+      <>
+        {['/', '/folder/:folderId'].map(path =>
+          <Route
+            exact
+            key={path}
+            path={path}
+            component={NoteListNav}
+          />
+        )}
+        <Route
+          path='/note/:noteId'
+          component={NotePageNav}
+        />
+        <Route
+          path='/add-folder'
+          component={NotePageNav}
+        />
+        <Route
+          path='/add-note'
+          component={NotePageNav}
+        />
+      </>
+    )
+  }
+
+  renderMainRoutes() {
+    return (
+      <>
+        {['/', '/folder/:folderId'].map(path =>
+          <Route
+            exact
+            key={path}
+            path={path}
+            component={NoteListMain}
+          />
+        )}
+        <Route
+          path='/note/:noteId'
+          component={NotePageMain}
+        />
+        <Route
+          path='/add-folder'
+          component={AddFolder}
+        />
+        <Route
+          path='/add-note'
+          component={AddNote}
+        />
+      </>
+    )
+  }
 
   render() {
+    const value = {
+      notes: this.state.notes,
+      folders: this.state.folders,
+      addFolder: this.handleAddFolder,
+      addNote: this.handleAddNote,
+      deleteNote: this.handleDeleteNote,
+    }
     return (
-      <Context.Provider value={{
-        ...this.state,
-        handleDeleteBookmark: this.handleDeleteBookmark,
-        handleDeleteFolder: this.handleDeleteFolder,
-        addFolder: this.addFolder,
-        addBookmark: this.addBookmark,
-        handleUpdateFolder: this.handleUpdateFolder
-      }}>
-        <header id="SiteTitle" role="banner">
-          <Link to='/'>Nebs</Link>
-        </header>
-        <ErrorPage>
-          <nav role="navigation">
-            {this.renderNavigationComponent()}
+      <ApiContext.Provider value={value}>
+        <div className='App'>
+          <nav className='App__nav'>
+            {this.renderNavRoutes()}
           </nav>
-          <main role="main">
-            {this.renderMainComponent()}
+          <header className='App__header'>
+            <h1>
+              <Link to='/'>Noteful</Link>
+              {' '}
+              <FontAwesomeIcon icon='check-double' />
+            </h1>
+          </header>
+          <main className='App__main'>
+            {this.renderMainRoutes()}
           </main>
-        </ErrorPage>
-      </Context.Provider>
-    );
+        </div>
+      </ApiContext.Provider>
+    )
   }
 }
 
-export default App;
+export default App
+
---
+++ src/components/AddBookmark/AddBookmark.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import NebsForm from '../NebsForm/NebsForm'
+import Context from '../Context'
+import config from '../config'
+import './AddBookmark.css'
+
+export default class AddBookmark extends Component {
+    static defaultProps = {
+        history: {
+            push: () => { }
+        },
+    }
+    static contextType = ApiContext;
+
+    handleSubmit = e => {
+        e.preventDefault()
+        const newBookmark = {
+            name: e.target['bookmark-name'].value,
+            content: e.target['bookmark-content'].value,
+            folder_id: e.target['bookmark-folder-id'].value,
+            modified: new Date(),
+        }
+        fetch(`${config.API_ENDPOINT}/bookmarks`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newBookmark),
+        })
+            .then(res => {
+                if (!res.ok)
+                    return res.json().then(e => Promise.reject(e))
+                return res.json()
+            })
+            .then(bookmark => {
+                this.context.addBookmark(bookmark)
+                this.props.history.push(`/folder/${bookmark.folder_id}`)
+            })
+            .catch(error => {
+                console.error({ error })
+            })
+    }
+
+    render() {
+        const { folders = [] } = this.context
+        return (
+            <section className='AddBookmark'>
+                <h2>Create a bookmark</h2>
+                <NebsForm onSubmit={this.handleSubmit}>
+                    <div className='field'>
+                        <label htmlFor='bookmark-name-input'>
+                            Name
+            </label>
+                        <input type='text' id='bookmark-name-input' name='bookmark-name' />
+                    </div>
+                    <div className='field'>
+                        <label htmlFor='bookmark-content-input'>
+                            Content
+            </label>
+                        <textarea id='bookmark-content-input' name='bookmark-content' />
+                    </div>
+                    <div className='field'>
+                        <label htmlFor='bookmark-folder-select'>
+                            Folder
+            </label>
+                        <select id='bookmark-folder-select' name='bookmark-folder-id'>
+                            <option value={null}>...</option>
+                            {folders.map(folder =>
+                                <option key={folder.id} value={folder.id}>
+                                    {folder.name}
+                                </option>
+                            )}
+                        </select>
+                    </div>
+                    <div className='buttons'>
+                        <button type='submit'>
+                            Add Bookmark
+            </button>
+                    </div>
+                </NebsForm>
+            </section>
+        )
+    }
+}
